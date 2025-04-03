d0b2983fe6a23d0c48f687fefbb3e0f41133d5fa
Paritosh Srivastava <srivastavaparitosh1000@gmail.com>
2025-04-03 21:39:17 +0530

    UI changes P

--- app/universities/page.tsx
+++ app/universities/page.tsx
@@ -5,6 +5,7 @@ import { useEffect, useState } from "react";
 import { createClient } from "@supabase/supabase-js";
 import UniversityCard from "../../components/ui/UniversityCard";
 import UniversityDetailModal from "../../components/ui/UniversityDetailModal";
+import FloatingNavbar from "../../components/Navbar";
 
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
@@ -32,6 +33,14 @@ export default function UniversitiesPage() {
   // States for search and sort
   const [searchTerm, setSearchTerm] = useState("");
   const [sortOption, setSortOption] = useState("name-asc");
+
+  const dashboardLinks = [
+    { href: "/roadmap", label: "Roadmap" },
+    { href: "/events", label: "Events" },
+    { href: "/analytics", label: "User Analysis" },
+    { href: "/jobs", label: "Jobs" },
+    { href: "/universities", label: "Universities" },
+  ];
 
   // Fetch all universities once
   useEffect(() => {
@@ -85,24 +94,27 @@ export default function UniversitiesPage() {
 
   return (
     <div className="container mx-auto p-36">
-      <h1 className="text-3xl text-black font-bold mb-6">Universities</h1>
+      <FloatingNavbar navLinks={dashboardLinks} />
+      <h1 className="text-5xl text-[#FF6500] font-bold mb-6 text-center">
+        Universities
+      </h1>
 
       {/* Search & Sort controls */}
-      <div className="flex flex-col sm:flex-row items-start sm:items-center mb-6 gap-4">
+      <div className="flex flex-col mb-16 sm:flex-row justify-center items-start sm:items-center gap-4">
         {/* Search bar */}
         <input
           type="text"
           placeholder="Search by name or country"
           value={searchTerm}
           onChange={(e) => setSearchTerm(e.target.value)}
-          className="border border-gray-300 px-3 py-2 rounded w-full sm:w-auto"
+          className="border border-gray-300 px-3 py-2 text-gray-800 rounded-lg w-full lg:w-1/3"
         />
 
         {/* Sort dropdown */}
         <select
           value={sortOption}
           onChange={(e) => setSortOption(e.target.value)}
-          className="border border-gray-300 text-black px-3 py-2 rounded"
+          className="border border-gray-300 text-gray-800 px-3 py-2 rounded-lg w-full lg:w-1/4"
         >
           <option value="name-asc">Name (A-Z)</option>
           <option value="name-desc">Name (Z-A)</option>
