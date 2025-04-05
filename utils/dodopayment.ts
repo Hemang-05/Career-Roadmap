214dcd81bc17a6196c5cf8a44df46b0425d9c348
Hemang Mehta <mehta2000dec@gmail.com>
2025-04-05 22:52:12 +0530

    Before deployment 9 dodo

--- utils/dodopayment.ts
+++ utils/dodopayment.ts
@@ -5,5 +5,5 @@ export const dodopayments = new DodoPayments({
       ? process.env.DODO_API_KEY_TEST
       : process.env.DODO_API_KEY_LIVE, // This is the default and can be omitted if env is named as DODO_PAYMENTS_API_KEY
   environment:
-    process.env.NODE_ENV === "development" ? "test_mode" : "live_mode", // defaults to 'live_mode'
+    process.env.NODE_ENV === "development" ? "test_mode" : "test_mode", // defaults to 'live_mode'
 });
