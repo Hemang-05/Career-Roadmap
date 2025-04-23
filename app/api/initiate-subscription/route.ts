5a277bd1ba8d35c2b92dfd3c0c9e39f6cf27b707
Hemang Mehta <61517187+Hemang-05@users.noreply.github.com>
2025-04-23 15:22:27 +0530

    Update route.ts

--- app/api/initiate-subscription/route.ts
+++ app/api/initiate-subscription/route.ts
@@ -71,6 +71,8 @@ export async function POST(request: Request) {
       },
       payment_link: true,
       product_id,
+      discount_id: "dsc_8gxTbEaGIXewLWzqsSYk5",
+      code: "",
       quantity: 1,
       return_url: process.env.NEXT_PUBLIC_RETURN_URL, // Adjust as needed
     });
